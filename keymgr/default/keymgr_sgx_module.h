60a23e7f928254bec1c2a815ad2fc2c69f8e4280
AthrunArthur <athrunarthur@gmail.com>
2021-05-07 16:26:45 +0800

    remove report gen in keymgr

--- keymgr/default/keymgr_sgx_module.h
+++ keymgr/default/keymgr_sgx_module.h
@@ -63,12 +63,4 @@ public:
                            uint32_t ehash_size, const uint8_t *verify_key,
                            uint32_t vpkey_size, const uint8_t *sig,
                            uint32_t sig_size);
-  uint32_t create_report_for_pkey(const sgx_target_info_t *p_qe3_target,
-                                  const stbox::bytes &pkey,
-                                  sgx_report_t *p_report);
-
-  uint32_t verify_report_and_sign(const sgx_report_t *p_report,
-                                  const stbox::bytes &pkey,
-                                  const stbox::bytes &sig_pkey,
-                                  stbox::bytes &sig);
 };
