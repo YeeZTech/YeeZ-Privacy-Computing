990054e1fe30469bb64fd35281b4df9852580574
Xuepeng <athrunarthur@gmail.com>
2021-05-17 18:06:43 +0800

    revise group

--- hpda/include/hpda/processor/transform/group.h
+++ hpda/include/hpda/processor/transform/group.h
@@ -15,26 +15,104 @@ public:
 
   void begin_group() { m_data = 0; }
   template <typename T> void group(const T &t) {
-    m_data += t.get<InputObjType>();
+    m_data += t.template get<InputColumnType>();
   }
   void end_group() {}
-  typename output_type::value_type output() const { return m_data; }
+  typename ::ff::util::internal::nt_traits<output_type>::type output() const {
+    return m_data;
+  }
 
 protected:
-  typename OutputColumnType::value_type m_data;
+  typename ::ff::util::internal::nt_traits<OutputColumnType>::type m_data;
 };
 
 template <typename InputColumnType, typename OutputColumnType>
-class avg : public aggregator_base<InputColumnType, OutputColumnType> {};
+class avg : public aggregator_base<InputColumnType, OutputColumnType> {
+public:
+  typedef InputColumnType input_type;
+  typedef OutputColumnType output_type;
+
+  void begin_group() {
+    m_data = 0;
+    m_count = 0;
+  }
+  template <typename T> void group(const T &t) {
+    m_data += t.template get<InputColumnType>();
+    m_count += 1;
+  }
+  void end_group() {
+    if (m_count == 0) {
+      return;
+    }
+    m_data = m_data / m_count;
+  }
+  typename ::ff::util::internal::nt_traits<output_type>::type output() const {
+    return m_data;
+  }
+
+protected:
+  typename ::ff::util::internal::nt_traits<OutputColumnType>::type m_data;
+  size_t m_count;
+};
 
 template <typename InputColumnType, typename OutputColumnType>
-class max : public aggregator_base<InputColumnType, OutputColumnType> {};
+class max : public aggregator_base<InputColumnType, OutputColumnType> {
+public:
+  typedef InputColumnType input_type;
+  typedef OutputColumnType output_type;
+
+  void begin_group() { m_data = 0; }
+  template <typename T> void group(const T &t) {
+    auto d = t.template get<InputColumnType>();
+    m_data = m_data > d ? m_data : d;
+  }
+  void end_group() {}
+  typename ::ff::util::internal::nt_traits<output_type>::type output() const {
+    return m_data;
+  }
+
+protected:
+  typename ::ff::util::internal::nt_traits<OutputColumnType>::type m_data;
+};
 
 template <typename InputColumnType, typename OutputColumnType>
-class min : public aggregator_base<InputColumnType, OutputColumnType> {};
+class min : public aggregator_base<InputColumnType, OutputColumnType> {
+public:
+  typedef InputColumnType input_type;
+  typedef OutputColumnType output_type;
+
+  void begin_group() { m_data = 0; }
+  template <typename T> void group(const T &t) {
+    auto d = t.template get<InputColumnType>();
+    m_data = m_data < d ? m_data : d;
+  }
+  void end_group() {}
+  typename ::ff::util::internal::nt_traits<output_type>::type output() const {
+    return m_data;
+  }
+
+protected:
+  typename ::ff::util::internal::nt_traits<OutputColumnType>::type m_data;
+};
 
 template <typename InputColumnType, typename OutputColumnType>
-class count : public aggregator_base<InputColumnType, OutputColumnType> {};
+class count : public aggregator_base<InputColumnType, OutputColumnType> {
+public:
+  typedef InputColumnType input_type;
+  typedef OutputColumnType output_type;
+
+  void begin_group() { m_data = 0; }
+  template <typename T> void group(const T &t) {
+    m_data += 1;
+  }
+  void end_group() {}
+  typename ::ff::util::internal::nt_traits<output_type>::type output() const {
+    return m_data;
+  }
+
+protected:
+  typename ::ff::util::internal::nt_traits<OutputColumnType>::type m_data;
+};
 
 template <typename InputObjType, typename OutputObjType, typename GroupByType,
           typename AggregatorType>
@@ -42,35 +120,70 @@ class groupby_impl : public processor_base<InputObjType, OutputObjType> {
 public:
   groupby_impl(
       ::hpda::internal::processor_with_output<InputObjType> *upper_stream,
-      const GroupByType &group_column, const AggregatorType &aggregator)
-      : processor_base<InputObjType, OutputObjType>(upper_stream) {}
+      const AggregatorType &aggregator)
+      : processor_base<InputObjType, OutputObjType>(upper_stream),
+        m_aggregator(aggregator), m_continue(false) {}
   virtual ~groupby_impl() {}
 
   typedef processor_base<InputObjType, OutputObjType> base;
 
   virtual bool next_output() {
-    while (base::next_input()) {
-      GroupByType t = base::input_value().get<GroupByType>();
+    typename ::ff::util::internal::nt_traits<GroupByType>::type t;
+    if (m_continue || base::next_input()) {
+      t = base::input_value().template get<GroupByType>();
       m_last_group_id = t;
       m_aggregator.begin_group();
       m_aggregator.group(base::input_value());
       m_data = base::input_value();
-      while (m_last_group_id == t && base::next_input()) {
-        m_aggregator.group(base::input_value());
-      }
-      m_aggregator.end_group();
-      m_data.set<AggregatorType::output_type>(m_aggregator.output());
-      return true;
+    } else {
+      return false;
     }
-    return false;
+    while (base::next_input()) {
+      t = base::input_value().template get<GroupByType>();
+      if (m_last_group_id == t) {
+        m_aggregator.group(base::input_value());
+      } else {
+        break;
+      }
+    }
+    m_aggregator.end_group();
+    m_data.template set<typename AggregatorType::output_type>(
+        m_aggregator.output());
+
+    if (m_last_group_id != t) {
+      m_continue = true;
+    } else {
+      m_continue = false;
+    }
+    return true;
   }
-  virtual OutputObjType output_value() { return m_data; }
+  virtual OutputObjType output_value() {
+    return m_data.make_copy();
+  }
 
 protected:
-  GroupByType m_last_group_id;
-  OutputObjType m_value;
+  typename ::ff::util::internal::nt_traits<GroupByType>::type m_last_group_id;
+  OutputObjType m_data;
   AggregatorType m_aggregator;
+  bool m_continue;
 };
 } // namespace internal
+template <typename InputObjType, typename GroupByType, typename AggregatorType,
+          typename... ARGS>
+using groupby = internal::groupby_impl<InputObjType, ntobject<ARGS...>,
+                                       GroupByType, AggregatorType>;
+
+namespace group {
+template <typename InputObjType, typename OutputObjType>
+using sum = internal::sum<InputObjType, OutputObjType>;
+template <typename InputObjType, typename OutputObjType>
+using avg = internal::avg<InputObjType, OutputObjType>;
+template <typename InputObjType, typename OutputObjType>
+using max = internal::max<InputObjType, OutputObjType>;
+template <typename InputObjType, typename OutputObjType>
+using min = internal::min<InputObjType, OutputObjType>;
+template <typename InputObjType, typename OutputObjType>
+using count = internal::count<InputObjType, OutputObjType>;
+}
 } // namespace processor
 } // namespace hpda
---
+++ hpda/test/processor/test_groupby.cpp
@@ -0,0 +1,167 @@
+#include <gtest/gtest.h>
+#include <hpda/hpda.h>
+
+define_nt(hash, std::string);
+define_nt(value, uint64_t);
+define_nt(sum_v, uint64_t);
+typedef hpda::ntobject<hash, value> data_item_t;
+
+TEST(groupby, sum) {
+  hpda::extractor::raw_data<hash, value> rd;
+  int sum = 0;
+  for (int i = 0; i < 1000; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(100), i);
+    rd.add_data(d);
+    sum += i;
+  }
+  int sum2 = 0;
+  for (int i = 0; i < 10; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(10), i);
+    rd.add_data(d);
+    sum2 += i;
+  }
+
+  typedef hpda::processor::group::sum<value, sum_v> sum_t;
+  sum_t sum_aggregator;
+  hpda::processor::groupby<hpda::extractor::raw_data<hash, value>::output_type,
+                           hash, sum_t, hash, sum_v>
+      f(&rd, sum_aggregator);
+
+  hpda::output::memory_output<hash, sum_v> mo(&f);
+
+  mo.run();
+  auto s = mo.values().size();
+  EXPECT_EQ(s, 2);
+  EXPECT_EQ(mo.values()[0].get<sum_v>(), sum);
+  EXPECT_EQ(mo.values()[1].get<sum_v>(), sum2);
+}
+
+TEST(groupby, avg) {
+  hpda::extractor::raw_data<hash, value> rd;
+  int sum = 0;
+  for (int i = 0; i < 1000; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(100), i);
+    rd.add_data(d);
+    sum += i;
+  }
+  int sum2 = 0;
+  for (int i = 0; i < 10; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(10), i);
+    rd.add_data(d);
+    sum2 += i;
+  }
+
+  typedef hpda::processor::group::avg<value, sum_v> avg_t;
+  avg_t aggregator;
+  hpda::processor::groupby<hpda::extractor::raw_data<hash, value>::output_type,
+                           hash, avg_t, hash, sum_v>
+      f(&rd, aggregator);
+
+  hpda::output::memory_output<hash, sum_v> mo(&f);
+
+  mo.run();
+  auto s = mo.values().size();
+  EXPECT_EQ(s, 2);
+  EXPECT_EQ(mo.values()[0].get<sum_v>(), sum / 1000);
+  EXPECT_EQ(mo.values()[1].get<sum_v>(), sum2 / 10);
+}
+
+TEST(groupby, max) {
+  hpda::extractor::raw_data<hash, value> rd;
+  int sum = 0;
+  for (int i = 0; i < 1000; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(100), i);
+    rd.add_data(d);
+    sum += i;
+  }
+  int sum2 = 0;
+  for (int i = 0; i < 10; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(10), i);
+    rd.add_data(d);
+    sum2 += i;
+  }
+
+  typedef hpda::processor::group::max<value, sum_v> max_t;
+  max_t aggregator;
+  hpda::processor::groupby<hpda::extractor::raw_data<hash, value>::output_type,
+                           hash, max_t, hash, sum_v>
+      f(&rd, aggregator);
+
+  hpda::output::memory_output<hash, sum_v> mo(&f);
+
+  mo.run();
+  auto s = mo.values().size();
+  EXPECT_EQ(s, 2);
+  EXPECT_EQ(mo.values()[0].get<sum_v>(), 999);
+  EXPECT_EQ(mo.values()[1].get<sum_v>(), 9);
+}
+
+TEST(groupby, min) {
+  hpda::extractor::raw_data<hash, value> rd;
+  int sum = 0;
+  for (int i = 0; i < 1000; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(100), i);
+    rd.add_data(d);
+    sum += i;
+  }
+  int sum2 = 0;
+  for (int i = 0; i < 10; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(10), i);
+    rd.add_data(d);
+    sum2 += i;
+  }
+
+  typedef hpda::processor::group::min<value, sum_v> min_t;
+  min_t aggregator;
+  hpda::processor::groupby<hpda::extractor::raw_data<hash, value>::output_type,
+                           hash, min_t, hash, sum_v>
+      f(&rd, aggregator);
+
+  hpda::output::memory_output<hash, sum_v> mo(&f);
+
+  mo.run();
+  auto s = mo.values().size();
+  EXPECT_EQ(s, 2);
+  EXPECT_EQ(mo.values()[0].get<sum_v>(), 0);
+  EXPECT_EQ(mo.values()[1].get<sum_v>(), 0);
+}
+
+TEST(groupby, count) {
+  hpda::extractor::raw_data<hash, value> rd;
+  int sum = 0;
+  for (int i = 0; i < 10; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(100), i);
+    rd.add_data(d);
+    sum += i;
+  }
+  int sum2 = 0;
+  for (int i = 0; i < 10; i++) {
+    data_item_t d;
+    d.set<hash, value>(std::to_string(10), i);
+    rd.add_data(d);
+    sum2 += i;
+  }
+
+  typedef hpda::processor::group::count<value, sum_v> count_t;
+  count_t aggregator;
+  hpda::processor::groupby<hpda::extractor::raw_data<hash, value>::output_type,
+                           hash, count_t, hash, sum_v>
+      f(&rd, aggregator);
+
+  hpda::output::memory_output<hash, sum_v> mo(&f);
+
+  mo.run();
+  auto s = mo.values().size();
+  EXPECT_EQ(s, 2);
+  EXPECT_EQ(mo.values()[0].get<sum_v>(), 10);
+  EXPECT_EQ(mo.values()[1].get<sum_v>(), 10);
+}
