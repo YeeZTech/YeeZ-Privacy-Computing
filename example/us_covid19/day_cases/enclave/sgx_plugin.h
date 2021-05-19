5944072a55872b1fe0c8c9fff24002bd400b8fc6
chmwang <chmwang@zju.edu.cn>
2021-05-19 10:24:45 +0800

    add example of us covid-19 parser

---
+++ example/us_covid19/day_cases/enclave/sgx_plugin.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "user_type.h"
+
+namespace ypc {
+user_item_t ecall_parse_item_data(const uint8_t *data, size_t len);
+}
---
+++ example/us_covid19/plugin/us_covid19_reader.cpp
@@ -0,0 +1,60 @@
+#include "us_covid19_reader.h"
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+void *create_item_reader(const char *file_path, int len) {
+  std::ifstream *s = new std::ifstream(file_path);
+
+  return s;
+}
+
+int reset_for_read(void *handle) {
+  if (!handle) {
+    return -1;
+  }
+  std::ifstream *is = (std::ifstream *)handle;
+  is->clear();
+  return 0;
+}
+
+int read_item_data(void *handle, char *buf, int *len) {
+  if (!handle) {
+    return -1;
+  }
+  if (!buf) {
+    return -1;
+  }
+
+  std::string s;
+  std::ifstream *is = (std::ifstream *)handle;
+  std::getline(*is, s);
+  memcpy(buf, s.c_str(), s.size());
+  *len = s.size();
+
+  return 0;
+}
+
+int close_item_reader(void *handle) {
+  std::ifstream *is = (std::ifstream *)handle;
+  is->close();
+  return 0;
+}
+
+uint64_t get_item_number(void *handle) {
+  std::ifstream *is = (std::ifstream *)handle;
+  is->seekg(0, is->beg);
+  uint64_t n = 0;
+  std::string s = "s";
+  while (s.size() > 0 && !is->eof()) {
+    s.clear();
+    std::getline(*is, s);
+    if (s.size() > 0) {
+      n++;
+    }
+  }
+  is->seekg(0, is->beg);
+  return n;
+}
