75c4d158b45e385be638b36d6adfdb1f091c0fc0
Xuepeng <athrunarthur@gmail.com>
2021-10-18 19:27:15 +0800

    add mysql support

---
+++ test/toolkit/plugins/mysql/main.cpp
@@ -0,0 +1,63 @@
+#include "ff/sql/mysql.hpp"
+#include "ff/sql/table.h"
+#include "toolkit/plugins/mysql/mysql_reader.h"
+#include "ypc_t/analyzer/ntpackage_item_parser.h"
+#include <iostream>
+
+define_nt(v1, int);
+define_nt(s1, std::string);
+define_nt(v2, double);
+
+define_column(cv1, key, int, "v1");
+define_column(cs1, column, std::string, "s1");
+define_column(cv2, column, double, "v2");
+
+struct mymeta {
+  constexpr static const char *table_name = "xxxyyy";
+};
+typedef ff::sql::table<ff::sql::mysql<ff::sql::cppconn>, mymeta, cv1, cs1, cv2>
+    mytable;
+
+typedef mytable::row_collection_type::row_type mt;
+
+void generate_mysql_data(ff::sql::mysql<ff::sql::cppconn> &engine) {
+
+  try {
+    mytable::drop_table(&engine);
+  } catch (...) {
+    std::cout << "drop_table failed" << std::endl;
+  }
+  mytable::create_table(&engine);
+  mytable::row_collection_type rows;
+  mytable::row_collection_type::row_type t1;
+  t1.set<cv1, cs1, cv2>(1, "test", 2.3f);
+  rows.push_back(t1);
+  t1.set<cv1, cs1, cv2>(2, "test2", 2.4f);
+  rows.push_back(t1);
+  t1.set<cv1, cs1, cv2>(3, "test3", 3.4f);
+  rows.push_back(t1);
+  mytable::insert_or_replace_rows(&engine, rows);
+}
+
+int main(int argc, char *argv[]) {
+  ff::sql::mysql<ff::sql::cppconn> engine("tcp://127.0.0.1:3306", "root", "",
+                                          "test");
+  generate_mysql_data(engine);
+  // clang-format off
+  std::string json("{\"url\":\"tcp://127.0.0.1:3306\", \"username\":\"root\", \"password\":\"\", \"dbname\":\"test\"}");
+  // clang-format on
+  std::cout << json << std::endl;
+  ypc::plugins::typed_mysql_reader<mt, mytable> r(json);
+
+  std::cout << r.get_item_number() << std::endl;
+  int len;
+  r.read_item_data(nullptr, &len);
+  char *buf = new char[len];
+  r.read_item_data(buf, &len);
+  mt p = ypc::ntpackage_item_parser<char, mt>::parser(buf, len);
+  std::cout << p.get<cv1>() << ", " << p.get<cs1>() << ", " << p.get<cv2>()
+            << std::endl;
+
+  delete buf;
+  return 0;
+}
---
+++ toolkit/plugins/mysql/mysql_reader.h
@@ -0,0 +1,178 @@
+#pragma once
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <ff/net/middleware/ntpackage.h>
+#include <ff/sql/mysql.hpp>
+#include <ff/util/ntobject.h>
+#include <fstream>
+#include <memory>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void *create_item_reader(const char *extra_param, int len);
+
+int reset_for_read(void *handle);
+int read_item_data(void *handle, char *buf, int *len);
+int close_item_reader(void *handle);
+uint64_t get_item_number(void *handle);
+
+#ifdef __cplusplus
+}
+#endif
+
+namespace ypc {
+namespace plugins {
+class mysql_reader {
+public:
+  virtual int reset_for_read() = 0;
+  virtual int read_item_data(char *buf, int *len) = 0;
+  virtual int close_item_reader() = 0;
+  virtual int get_item_number() = 0;
+};
+
+template <typename T, typename Table>
+class typed_mysql_reader : public mysql_reader {
+public:
+  typedef T item_t;
+  template <typename NT> struct cast_obj_to_package {};
+  template <typename... ARGS>
+  struct cast_obj_to_package<::ff::util::ntobject<ARGS...>> {
+    typedef ::ff::net::ntpackage<0, ARGS...> type;
+  };
+
+  // TODO we should add more mysql options here
+  /// @extra_param should be a json string, like this
+  // {url: "xxx", username:"xxx", password:"xxx", dbname="xxx", table{name:"xx",
+  // schema:[]}}
+  typed_mysql_reader(const std::string &extra_param)
+      : m_extra_param(extra_param) {
+    boost::property_tree::ptree pt;
+    std::stringstream ss;
+    ss << extra_param;
+    boost::property_tree::json_parser::read_json(ss, pt);
+
+    auto url = pt.get_child("url").get_value<std::string>();
+    auto username = pt.get_child("username").get_value<std::string>();
+    auto password = pt.get_child("password").get_value<std::string>();
+    auto dbname = pt.get_child("dbname").get_value<std::string>();
+    m_engine.reset(
+        new ff::sql::mysql<ff::sql::cppconn>(url, username, password, dbname));
+  }
+
+  virtual int reset_for_read() {
+    m_to_read_index = 0;
+    return 0;
+  }
+  template <typename NTObjType> struct read_helper {};
+  template <typename... ARGS>
+  struct read_helper<::ff::util::ntobject<ARGS...>> {
+    template <typename TableT, typename EngineT, typename RetT>
+    static void read_item_data(EngineT *engine, RetT &ret) {
+      ret = TableT::template select<ARGS...>(engine).eval();
+    }
+  };
+
+  template <typename... ARGS> struct assign_helper {};
+  template <typename ARGT> struct assign_helper<ARGT> {
+    template <typename RT, typename ST>
+    static void assign(RT &to, const ST &from) {
+      to.template set<ARGT>(from.template get<ARGT>());
+    }
+  };
+
+  template <typename ARGT1, typename ARGT2, typename... ARGS>
+  struct assign_helper<ARGT1, ARGT2, ARGS...> {
+    template <typename RT, typename ST>
+    static void assign(RT &to, const ST &from) {
+      to.template set<ARGT1>(from.template get<ARGT1>());
+      assign_helper<ARGT2, ARGS...>::assign(to, from);
+    }
+  };
+  template <typename RT> struct assign_package {};
+  template <typename... ARGS>
+  struct assign_package<::ff::util::ntobject<ARGS...>> {
+    template <typename PT>
+    static void to(PT &_to, const ::ff::util::ntobject<ARGS...> &from) {
+      assign_helper<ARGS...>::assign(_to, from);
+    }
+  };
+
+  void
+  read_all_items() {
+    read_helper<item_t>::template read_item_data<Table>(m_engine.get(),
+                                                        m_all_items);
+  }
+  virtual int read_item_data(char *buf, int *len) {
+    typedef typename cast_obj_to_package<item_t>::type package_t;
+    if (m_all_items.empty()) {
+      read_all_items();
+    }
+
+    if (m_to_read_index >= m_all_items.size()) {
+      return -1;
+    }
+    package_t v;
+    assign_package<item_t>::to(v, m_all_items[m_to_read_index]);
+    m_to_read_index++;
+
+    if (len) {
+      ff::net::marshaler lm(ff::net::marshaler::length_retriver);
+      v.archive(lm);
+      *len = static_cast<int>(lm.get_length());
+    }
+    if (buf) {
+      ff::net::marshaler sm(buf, *len, ff::net::marshaler::seralizer);
+      v.archive(sm);
+    }
+    return 0;
+  }
+
+  virtual int close_item_reader() {
+    m_all_items.clear();
+    return 0;
+  }
+  virtual int get_item_number() { return static_cast<int>(m_all_items.size()); }
+
+protected:
+  const std::string m_extra_param;
+  std::unique_ptr<::ff::sql::mysql<ff::sql::cppconn>> m_engine;
+  typename Table::row_collection_type m_all_items;
+  // std::vector<item_t> m_all_items;
+  size_t m_to_read_index;
+};
+} // namespace plugins
+} // namespace ypc
+
+#define impl_mysql_reader(type)                                                \
+  void *create_item_reader(const char *extra_param, int len) {                 \
+    try {                                                                      \
+      ypc::plugins::mysql_reader *reader =                                     \
+          new type(std::string(extra_param, len));                             \
+      return reader;                                                           \
+    } catch (const std::exception &e) {                                        \
+      return nullptr;                                                          \
+    }                                                                          \
+  }                                                                            \
+  int reset_for_read(void *handle) {                                           \
+    ypc::plugins::mysql_reader *reader = (ypc::plugins::csv_reader *)handle;   \
+    return reader->reset_for_read();                                           \
+  }                                                                            \
+  int read_item_data(void *handle, char *buf, int *len) {                      \
+    ypc::plugins::mysql_reader *reader = (ypc::plugins::csv_reader *)handle;   \
+    return reader->read_item_data(buf, len);                                   \
+  }                                                                            \
+  int close_item_reader(void *handle) {                                        \
+    ypc::plugins::mysql_reader *reader = (ypc::plugins::csv_reader *)handle;   \
+    reader->close_item_reader();                                               \
+    delete reader;                                                             \
+    return 0;                                                                  \
+  }                                                                            \
+                                                                               \
+  uint64_t get_item_number(void *handle) {                                     \
+    ypc::plugins::mysql_reader *reader = (ypc::plugins::csv_reader *)handle;   \
+    return reader->get_item_number();                                          \
+  }
+
