65c0064a438c12d3fb649bf6353410f33d3a8e8d
Wang Chenmin <chmwang@zju.edu.cn>
2022-08-24 17:36:23 +0800

    fix bug of eth hash prefix

--- ypc/core/src/corecommon/crypto/eth_hash.cpp
+++ ypc/core/src/corecommon/crypto/eth_hash.cpp
@@ -44,7 +44,11 @@ uint32_t eth_hash::get_msg_hash_size() { return 32; }
 
 uint32_t eth_hash::msg_hash(const uint8_t *raw_msg, uint32_t msg_size,
                             uint8_t *hash, uint32_t hash_size) {
-  const static char *prefix = "\x19Ethereum Signed Message:\n32";
+  // prefix string "\x19Ethereum Signed Message:\n32"
+  const static char prefix[] = {0x19, 0x45, 0x74, 0x68, 0x65, 0x72, 0x65,
+                                0x75, 0x6d, 0x20, 0x53, 0x69, 0x67, 0x6e,
+                                0x65, 0x64, 0x20, 0x4d, 0x65, 0x73, 0x73,
+                                0x61, 0x67, 0x65, 0x3a, 0x0a, 0x33, 0x32};
   const uint32_t l = strlen(prefix);
   bytes msg(l + 32);
   memcpy(msg.data(), prefix, l);
