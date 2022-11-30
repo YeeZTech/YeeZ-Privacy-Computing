23ed4c79caf9f298dbe0c13971d624bcff7b2d4f
AthrunArthur <athrunarthur@gmail.com>
2022-11-30 19:44:33 +0800

    revise

--- cmake/GTest.cmake
+++ cmake/GTest.cmake
@@ -17,8 +17,7 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 option(INSTALL_GMOCK "Install GMock" OFF)
 option(INSTALL_GTEST "Install GTest" ON)
 
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-  FetchContent_MakeAvailable(googletest)
-endif()
+FetchContent_MakeAvailable(googletest)
+
 include(GoogleTest)
 
