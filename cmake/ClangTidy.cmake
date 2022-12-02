b1ca10c6ac34a1ddc2c6087a9dc50d04095330a3
AthrunArthur <athrunarthur@gmail.com>
2022-12-02 10:28:19 +0800

    add miss file

---
+++ cmake/ClangTidy.cmake
@@ -0,0 +1,10 @@
+
+function(AddClangTidy target)
+  find_program(CLANG-TIDY_PATH clang-tidy REQUIRED)
+  set_target_properties(${target}
+    PROPERTIES CXX_CLANG_TIDY
+    "${CLANG-TIDY_PATH};-checks=*;--warnings-as-errors=*"
+    )
+endfunction()
+
+
