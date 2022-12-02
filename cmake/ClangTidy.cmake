a749d2f1043db121e477b0e5c49750fa86ac161c
程司时 <11570173+cheng-sishi@user.noreply.gitee.com>
2022-12-02 17:41:29 +0800

    modify clang tidy

--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -1,9 +1,9 @@
 
 function(AddClangTidy target)
   find_program(CLANG-TIDY_PATH clang-tidy REQUIRED)
-  set_target_properties(${target}
-    PROPERTIES CXX_CLANG_TIDY
-    "${CLANG-TIDY_PATH};-checks=*;--warnings-as-errors=*"
+  #set_target_properties(${target}
+    #PROPERTIES CXX_CLANG_TIDY
+    #"${CLANG-TIDY_PATH};-checks=*;--warnings-as-errors=*"
     )
 endfunction()
 
