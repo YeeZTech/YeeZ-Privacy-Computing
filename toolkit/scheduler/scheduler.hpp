05f01bd1f14a429c69106a8bff1e3fa54a2122ff
GAO Weihe <gao.weihe@outlook.com>
2023-09-15 14:37:47 +0800

    add run taskgraph logic

--- toolkit/scheduler/scheduler.hpp
+++ toolkit/scheduler/scheduler.hpp
@@ -16,6 +16,12 @@ namespace cluster {
     public:
         Scheduler() {
             dispatcher_ = std::make_unique<TaskDispatcher>();
+            waiting_task_map = std::make_shared<std::map<
+                    std::string, std::shared_ptr<tf::Taskflow>
+                    >>();
+            pending_task_map = std::make_shared<std::map<
+                    std::string, std::shared_ptr<tf::Taskflow>
+                    >>();
         }
 
     public:
@@ -27,43 +33,57 @@ namespace cluster {
             return data;
         }
 
+    private:
         void resolve_taskgraph(const nlohmann::json& json)
         {
             TaskGraph_SP tg = std::make_shared<TaskGraph>();
 
-            insert_taskgraph("", tg);
+            insert_taskgraph(json["name"], tg);
         }
 
+        // deprecated
         void run_taskgraph(TaskGraph_SP tg)
         {
+            std::unique_lock wtm_ul_(wtm_mutex_1_);
+            std::unique_lock ptm_ul_(ptm_mutex_2_);
             dispatcher_->run(*tg).wait();
         }
 
-        void run_taskgraph(const std::string& name)
-        {
+        void run_taskgraph(const std::string& name) {
+            {
+                std::unique_lock wtm_ul_(wtm_mutex_1_);
+                std::unique_lock ptm_ul_(ptm_mutex_2_);
+                auto tg_it = waiting_task_map->find(name);
+                pending_task_map->insert(std::make_pair(tg_it->first, tg_it->second));
+            }
 
+            {
+                std::unique_lock ptm_ul_(ptm_mutex_2_);
+                auto tg_it = pending_task_map->find(name);
+                dispatcher_->run(*(tg_it->second)).wait();
+                pending_task_map->erase(tg_it);
+            }
         }
 
-    private: 
         void insert_taskgraph(std::string name, TaskGraph_SP tg)
         {
-            std::unique_lock tm_ul_(waiting_task_map_mutex_);
+            std::unique_lock tm_ul_(wtm_mutex_1_);
             waiting_task_map->insert(
-                    std::pair<std::string, std::shared_ptr<tf::Taskflow>>(name, tg)
+                    std::pair<std::string, TaskGraph_SP>(name, tg)
                     );
         }
 
     private:
         std::shared_ptr<std::map<
             std::string,
-            std::shared_ptr<tf::Taskflow>
+            TaskGraph_SP
             >> waiting_task_map;
-        mutable std::shared_mutex waiting_task_map_mutex_;
+        mutable std::shared_mutex wtm_mutex_1_;
         std::shared_ptr<std::map<
-                std::string,
-                std::shared_ptr<tf::Taskflow>
-        >> pending_task_map;
-        mutable std::shared_mutex pending_task_map_mutex_;
+            std::string,
+            TaskGraph_SP
+            >> pending_task_map;
+        mutable std::shared_mutex ptm_mutex_2_;
 
         std::unique_ptr<TaskDispatcher> dispatcher_;
     }; 
