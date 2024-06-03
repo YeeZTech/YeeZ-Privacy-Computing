cdba3e09d084647ab73972f17288c3aceead34ca
lmjdusanzis <2529980619@qq.com>
2024-06-03 17:18:17 +0800

    include/ypc/core_t/analyzer/interface/data_interface.h

--- include/ypc/core_t/analyzer/interface/data_interface.h
+++ include/ypc/core_t/analyzer/interface/data_interface.h
@@ -5,10 +5,12 @@
 #include "ypc/core_t/analyzer/internal/data_streams/raw_data_stream.h"
 #include "ypc/core_t/analyzer/internal/data_streams/sealed_data_stream.h"
 #include "ypc/core_t/analyzer/internal/data_streams/oram_sealed_data_stream.h"
+#include "ypc/core_t/analyzer/internal/data_streams/convert_sealed_data_stream.h"
 #include "ypc/core_t/analyzer/internal/keymgr_session.h"
 #include "ypc/core_t/analyzer/raw_data_provider.h"
 #include "ypc/core_t/analyzer/sealed_data_provider.h"
 #include "ypc/core_t/analyzer/oram_sealed_data_provider.h"
+#include "ypc/core_t/analyzer/convert_sealed_data_provider.h"
 #include "ypc/core_t/analyzer/var/data_source_var.h"
 #include "ypc/corecommon/nt_cols.h"
 #include "ypc/corecommon/package.h"
@@ -165,6 +167,57 @@ public:
   }
 };
 
+template <typename Crypto>
+class data_interface<Crypto, convert_sealed_data_stream>
+    : virtual public data_source_var<convert_sealed_data_stream>,
+      virtual public keymgr_interface<Crypto>,
+      virtual public keymgr_session {
+  typedef keymgr_interface<Crypto> keymgr_interface_t;
+
+public:
+  uint32_t init_data_source(const uint8_t *data_source_info, uint32_t len) {
+    using ntt = nt<stbox::bytes>;
+    auto pkg = make_package<typename cast_obj_to_package<
+        ntt::sealed_data_info_t>::type>::from_bytes(data_source_info, len);
+
+    auto ret = keymgr_session::init_keymgr_session_convert();
+    if (ret) {
+      LOG(ERROR) << "init_keymgr_session_convert failed: " << stbox::status_string(ret);
+      return ret;
+    }
+
+    stbox::bytes private_key, dian_pkey;
+    ret = keymgr_interface_t::request_private_key_for_public_key(
+        pkg.get<ntt::pkey>(), private_key, dian_pkey);
+    if (ret) {
+      LOG(ERROR) << "request_private_key_for_public_key failed: "
+                 << stbox::status_string(ret);
+      return ret;
+    }
+
+    m_ds_use_pkey = pkg.get<ntt::pkey>() + pkg.get<ntt::data_hash>();
+    
+    m_datasource.reset(new convert_sealed_data_provider<Crypto>(
+        pkg.get<ntt::data_hash>(), private_key));
+    // m_datasource.reset(new sealed_data_provider<Crypto>(
+    //     pkg.get<ntt::data_hash>(), private_key));
+    
+
+    return stbox::stx_status::success;
+  }
+
+  uint32_t check_actual_data_hash() {
+    auto p = m_datasource.get();
+    if (p->data_hash() != p->expect_data_hash()) {
+      LOG(ERROR) << "expect " << p->expect_data_hash() << ", got "
+                 << p->data_hash();
+      return stbox::stx_status::data_hash_not_same_as_expect;
+    }
+    return stbox::stx_status::success;
+  }
+};
+
+// TODO:让多数据源也能使用ORAM
 template <typename Crypto>
 class data_interface<Crypto, multi_data_stream>
     : virtual public data_source_var<multi_data_stream>,
