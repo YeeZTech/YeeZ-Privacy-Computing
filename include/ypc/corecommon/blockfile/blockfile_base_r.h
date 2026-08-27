213ca055d2634d054c254fe0cc8b7da6f1728873
chmwang <chmwang@zju.edu.cn>
2026-08-27 15:53:21 +0800

    handle empty sealed file

--- include/ypc/corecommon/blockfile/blockfile_base_r.h
+++ include/ypc/corecommon/blockfile/blockfile_base_r.h
@@ -151,11 +151,13 @@ public:
   }
 
   virtual void close() {
-    // write block info
-    auto &back = m_block_infos.back();
-    m_file.seekp(back.end_file_pos, ftt::beg);
-    for (auto &bi : m_block_infos) {
-      m_file.write((char *)&bi, sizeof(bi));
+    if (!m_block_infos.empty()) {
+      // write block info
+      auto &back = m_block_infos.back();
+      m_file.seekp(back.end_file_pos, ftt::beg);
+      for (auto &bi : m_block_infos) {
+        m_file.write((char *)&bi, sizeof(bi));
+      }
     }
     // write header
     m_file.write((char *)&m_header, sizeof(m_header));
